dbc14059cc129dbc46cde2959b448f917d82c13b
Wamiq <wamiqwebs@gmail.com>
2026-06-16 16:30:00 +0500

    Fix PDF base URL

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -44,7 +44,7 @@ export async function POST(req: Request) {
 
     const baseUrl = !process.env.VERCEL
       ? "http://localhost:3000"
-      : "https://resume-builder-git-main-wamiqwebs-projects.vercel.app";
+      : "https://resume-builder-sand-omega-38.vercel.app";
 
     const url = `${baseUrl}/resume-preview?selected=${selected}&theme=${theme}&data=${encodeURIComponent(
       JSON.stringify(body.data)
